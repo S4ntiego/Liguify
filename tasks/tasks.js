fa2b1ab88549d04dffa8429bf12e9f33048ef274
Adam Ksiazek <adamksiazek123@gmail.com>
2023-06-28 10:19:20 +0200

    Add files via upload - Server

---
+++ tasks/tasks.js
@@ -0,0 +1,39 @@
+// const nodeCron = require("node-cron");
+// const fetchChallengerPlayersLeague = require("../functions/GLOBAL/Challenger/fetchChallengerPlayersLeague");
+// const fetchGrandmasterPlayersLeague = require("../functions/GLOBAL/Grandmaster/fetchGrandmasterPlayersLeague");
+// const fetchMasterPlayersLeague = require("../functions/GLOBAL/Master/fetchMasterPlayersLeague");
+
+// module.exports = () => {
+//   nodeCron.schedule("*/30 * * * *", () => {
+//     fetchChallengerPlayersLeague("EUN1");
+//     fetchChallengerPlayersLeague("NA1");
+//     fetchChallengerPlayersLeague("KR");
+//     fetchChallengerPlayersLeague("EUW1");
+//     fetchChallengerPlayersLeague("BR1");
+//     fetchChallengerPlayersLeague("TR1");
+//     fetchChallengerPlayersLeague("LA2");
+//     fetchChallengerPlayersLeague("LA1");
+//     fetchChallengerPlayersLeague("OC1");
+//     fetchChallengerPlayersLeague("JP1");
+//     fetchGrandmasterPlayersLeague("EUN1");
+//     fetchGrandmasterPlayersLeague("NA1");
+//     fetchGrandmasterPlayersLeague("KR");
+//     fetchGrandmasterPlayersLeague("EUW1");
+//     fetchGrandmasterPlayersLeague("BR1");
+//     fetchGrandmasterPlayersLeague("TR1");
+//     fetchGrandmasterPlayersLeague("LA2");
+//     fetchGrandmasterPlayersLeague("LA1");
+//     fetchGrandmasterPlayersLeague("OC1");
+//     fetchGrandmasterPlayersLeague("JP1");
+//     fetchMasterPlayersLeague("EUN1");
+//     fetchMasterPlayersLeague("NA1");
+//     fetchMasterPlayersLeague("KR");
+//     fetchMasterPlayersLeague("EUW1");
+//     fetchMasterPlayersLeague("BR1");
+//     fetchMasterPlayersLeague("TR1");
+//     fetchMasterPlayersLeague("LA2");
+//     fetchMasterPlayersLeague("LA1");
+//     fetchMasterPlayersLeague("OC1");
+//     fetchMasterPlayersLeague("JP1");
+//   });
+// };
